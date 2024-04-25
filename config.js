e4b8e0487b5e644fd4b41198fa8814b2065a1728
Yuni Jeong <seoyunjeong@umass.edu>
2024-04-25 14:49:59 -0400

    removing config file from gitignore

---
+++ config.js
@@ -0,0 +1,16 @@
+// OpenWeatherMap API key
+const weatherConfig = {
+	API_KEY: "786c26d249cfa35e4bdec273c6a4e5ae",
+};
+
+// Firebase configuration
+const firebaseConfig = {
+	apiKey: "AIzaSyCwsq64nmoXMwqdWEtsWFsHZYev0pe2fuQ",
+	authDomain: "samsara-backend.firebaseapp.com",
+	projectId: "samsara-backend",
+	storageBucket: "samsara-backend.appspot.com",
+	messagingSenderId: "120767486094",
+	appId: "1:120767486094:web:67b18af90099bf175f11df",
+};
+
+export { weatherConfig, firebaseConfig };
